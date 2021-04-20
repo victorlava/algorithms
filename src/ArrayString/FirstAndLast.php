fa329eb8709791690102a05730dc6c7a030402af
Victor Lava <victor.lava@aboutyou.com>
2021-04-20 11:55:15 +0300

    Binarysearch not finished for FirstAndLast

--- src/ArrayString/FirstAndLast.php
+++ src/ArrayString/FirstAndLast.php
@@ -21,7 +21,7 @@ class FirstAndLast {
         // TODO: this seems to work pretty fine, the time/space complexity should be 0(n),
         // Implement: binary search
         // Test the performance against binary search
-        
+
         $indices = [];
 
         for($i = 0; $i < count($nums);$i++) {
@@ -46,8 +46,55 @@ class FirstAndLast {
         return [$indices[0], $indices[count($indices) - 1]]; // Return the last index from the indices
     }
 
+    /**
+     * Sort the array as binary search only works on sorted ranges. Compute the middle element
+     * if the element we wish to search is greater than the middle element then search on the
+     * right side else search on the left. Return True if the element is found.
+     */
+    function binarySearch($nums, $target)
+    {
+
+        $begin = 0;
+        $end = count($nums) - 1;
+
+        while($begin <= $end) {
+
+            $middle = floor(($begin + $end) / 2);
+
+            if($nums[$middle] === $target) {
+                echo "found match" . ' ' . $nums[$middle] . PHP_EOL;
+                return $begin . ' ' . $end . PHP_EOL;
+
+            } elseif($nums[$middle] > $target) {
+//                echo "search left" . PHP_EOL;
+                $end = $middle - 1;
+//                echo $end . PHP_EOL;
+
+            } else {
+                echo 'search right' . PHP_EOL;
+                $begin = $middle + 1;
+            }
+
+        }
+        return -1;
+
+    }
+
 }
 
+
+$test = new FirstAndLast();
+print_R($test->binarySearch([5,7,7,8,8,9,10,10,11], 7));
+
+//
+//0 - 5
+//1 - 7
+//2 - 7
+//3 - 8
+//4 - 8
+//5 - 10
+//
+//6 / 2 = 3 - 1 = 2;
 
 //$test = new FirstAndLast();
 //print_R($test->searchAndFind([5,7,7,8,8,10], 8));
