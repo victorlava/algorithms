4efddc5c3a282cc8de8fee84ab9e205b65218877
Victor Lava <victor.lava@aboutyou.com>
2021-04-20 15:47:36 +0300

    Added some comments

--- src/ArrayString/Anagram.php
+++ src/ArrayString/Anagram.php
@@ -4,12 +4,25 @@ namespace Algorithm\ArrayString;
 
 class Anagram {
 
+
     /**
      * @param String[] $strs
      * @return String[][]
      */
-    function groupWithStandartFunctions($strs)
+    function groupWithStandardFunctions($strs)
     {
+        /** The initial idea was to go through each of the letters
+         *  in the word and calculate the byte or check sum. Then group words if sum is identical.
+         * However, the sum using ord() didn't work quite well, because
+         * there was a big chance of key collisions in the array, which meant some words
+         * might be put together even if they don't have common letters.
+         *
+         * This works much better. We basically sort the letters alphabetically, for ex. eat => aet, ate => aet, tea => aet
+         * and then if the sorted word matches we put them together in the same group.
+         * Would be nice to implement sort, implode and str_split method myself and compare the performance, but let's leave this for later.
+         */
+
+        //TODO: test the performance with phpunit
         $words = [];
 
         for($i = 0; $i < count($strs); $i++) {
@@ -44,7 +57,7 @@ class Anagram {
 
 
 $test = new Anagram();
-print_R($test->groupWithStandartFunctions(["eat","tea","tan","ate","nat","bat"])); // [["bat"],["nat","tan"],["ate","eat","tea"]]
+print_R($test->groupWithStandardFunctions(["eat","tea","tan","ate","nat","bat"])); // [["bat"],["nat","tan"],["ate","eat","tea"]]
 
 
 
