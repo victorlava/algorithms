afd59308c298ccadd565cffe1696346ae96b8f32
Victor Lava <victor.lava@aboutyou.com>
2021-04-21 15:14:48 +0300

    Small optimizations on iterative Fibonacci

--- src/Number/Fibonacci.php
+++ src/Number/Fibonacci.php
@@ -11,25 +11,18 @@ class Fibonacci {
     {
 
         $sequence = [0];
-
-        // Constraints: F0 = 0, F1 = 1
         if($numberOfCycles === 0) {
             return $sequence;
         }
 
+        $sequence[] = 1;
         if($numberOfCycles === 1) {
-            $sequence[] = $numberOfCycles;
             return $sequence;
         }
-        
-        $number = 1;
-        for($i = 0; $i < $numberOfCycles; $i++) {
-            if($i !== 0) {
-                $number += $sequence[$i - 1];
-            }
-            $sequence[] = $number;
-        }
 
+        for($i = 1; $i < $numberOfCycles; $i++) {
+            $sequence[] = $sequence[$i] + $sequence[$i - 1];
+        }
 
         return $sequence;
     }
@@ -38,9 +31,10 @@ class Fibonacci {
 
 
 $number = new Fibonacci();
-print_R($number->iterative(10));
-//print_R($number->iterative(0));
-//print_R($number->iterative(1));
+print_R($number->iterative(100));
+print_R($number->iterative(0));
+print_R($number->iterative(1));
+print_R($number->iterative(5));
 
 
 
