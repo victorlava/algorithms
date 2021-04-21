f9f0189b77f21047e71b4c25542582e31f520bd9
Victor Lava <victor.lava@aboutyou.com>
2021-04-21 14:46:43 +0300

    Implement fibonacci in an iterative way and recursive one

---
+++ src/Number/Fibonacci.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Algorithm\Number;
+
+class Fibonacci {
+
+
+    function recursive() {}
+
+    function iterative(int $numberOfCycles)
+    {
+
+
+
+
+    }
+
+}
+
+
+$number = new Fibonacci();
+$number->iterative();
+
+
+
