1767112108e5765515c35122f44a295c17ea83f4
Victor Lava <victor.lava@aboutyou.com>
2021-04-21 15:05:43 +0300

    Added iterative solution

--- src/Number/Fibonacci.php
+++ src/Number/Fibonacci.php
@@ -7,19 +7,40 @@ class Fibonacci {
 
     function recursive() {}
 
-    function iterative(int $numberOfCycles)
+    function iterative(int $numberOfCycles): array
     {
 
+        $sequence = [0];
+
+        // Constraints: F0 = 0, F1 = 1
+        if($numberOfCycles === 0) {
+            return $sequence;
+        }
+
+        if($numberOfCycles === 1) {
+            $sequence[] = $numberOfCycles;
+            return $sequence;
+        }
+        
+        $number = 1;
+        for($i = 0; $i < $numberOfCycles; $i++) {
+            if($i !== 0) {
+                $number += $sequence[$i - 1];
+            }
+            $sequence[] = $number;
+        }
 
 
-
+        return $sequence;
     }
 
 }
 
 
 $number = new Fibonacci();
-$number->iterative();
+print_R($number->iterative(10));
+//print_R($number->iterative(0));
+//print_R($number->iterative(1));
 
 
 
