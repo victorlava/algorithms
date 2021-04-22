6aa6416e3e1d5927146e774d8396f45aa3655e06
Victor Lava <victor.lava@aboutyou.com>
2021-04-22 21:18:02 +0300

    Actions to take to solve number of islands

--- src/Problem/NumberOfIslands.php
+++ src/Problem/NumberOfIslands.php
@@ -14,12 +14,20 @@ class NumberOfIslands {
 
     function getNumber() {
 
+        // 1. Loop through the array
+        // 2. Find the first 1.
+        // 3. Check if i has a neighbour on x: -1 and 1, also on y: -1 and 1
+        //  3.1 if doesn't have neighbour around it - add it to an array with lands and continue to find another 1
+        //  3.2 If 1 has a neighbour around then loop to the neighbour until there's no 1 and then add it to the land array
+        // 4. After this find another 1 and repeat step 3
+        // 5. If there are no 1 and 0 stop the cycle and count the lands in the land array.
+
     }
 
 }
 
 
-$test = new Consecutive();
+$test = new NumberOfIslands();
 $grid = [
     ["1","1","1","1","0"],
     ["1","1","0","1","0"],
