0060cdd928ee624dc95ef1198eddce8b7297ed1f
Victor Lava <victor.lava@aboutyou.com>
2021-04-26 19:21:29 +0300

    Added better solution for MergeInterval that compares the interval array with merged one overrides it

--- src/ArrayString/MergeInterval.php
+++ src/ArrayString/MergeInterval.php
@@ -18,28 +18,14 @@ class MergeInterval {
 
        usort($intervals, 'self::sort');
 
-       print_R($intervals);
-
        $merged = [];
+       foreach ($intervals as $interval) {
 
-       for($i = 0; $i < count($intervals); $i++) {
-
-           if(isset($intervals[$i + 1])) {
-               if($intervals[$i][1] >= $intervals[$i + 1][0]) { //overlap
-
-                   if($intervals[$i][1] > $intervals[$i + 1][1]) {
-                       $merged[] = $intervals[$i];
-                   } else {
-                       $merged[] = [$intervals[$i][0], $intervals[$i + 1][1]];
-                   }
-
-                   $i++;
-                   continue;
-               }
+           if(empty($merged) || end($merged)[1] < $interval[0]) { // if there's no merged values then add the value or if they are not overlapping
+                $merged[] = $interval;
+           } else { // overlapping
+               $merged[count($merged) - 1][1] = max(end($merged)[1], $interval[1]);
            }
-
-           $merged[] = $intervals[$i];
-
 
        }
 
@@ -50,7 +36,7 @@ class MergeInterval {
 
 
 $test = new MergeInterval();
-print_R($test->merge([[1,4],[0,2],[3,5]]));
+//print_R($test->merge([[1,4],[0,2],[3,5]]));
 //print_R($test->merge([[1,4],[2,3]]));
 //print_R($test->merge([[1,9],[2,5],[19,20],[10,11],[12,20],[0,3],[0,1]]));
 //print_R($test->merge([[1,3],[2,6],[8,10],[15,18]])); // [[1,6],[8,10],[15,18]], Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
