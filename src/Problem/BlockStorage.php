c3fd63a2e6ec9db2a8a6009f271cb8ae8c59dd71
Victor Lava <victor.lava@aboutyou.com>
2021-04-27 14:46:39 +0300

    Added BlockStorage problem

---
+++ src/Problem/BlockStorage.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Algorithm\Problem;
+
+/* In a block storage system new data is written in blocks. We are going to represent the flash memory as a one sequential array.
+We have a list of block writes coming in the form of arrays of size 2: writes[i] = [first_block_written, last_block_written].
+Each block has a rewrite limit. If rewrites on a block reach a certain specified threshold we should run a specific diagnostic.
+In a block storage system, new data is written in blocks. We are going to represent the flash memory as one sequential array. We have a list of block writes coming in the form of arrays of size 2: writes[i] = [first_block_written, last_block_written].
+
+Each block has a rewrite limit. If rewrites on a block reach a certain specified threshold we should run a special diagnostic.
+Given blockCount (an integer representing the total number of blocks), writes (the list of block-write arrays of size 2), and threshold,
+your task is to return the list of disjoint block segments, each consisting of blocks that have reached the rewrite threshold.
+The list of block segments should be sorted in increasing order by their left ends.
+ */
+
+
+class BlockStorage {
+
+    function getNumber(array $grid) {
+
+    }
+
+}
+
+
+
+
+
+
+
